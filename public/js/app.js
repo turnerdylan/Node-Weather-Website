894d315faa0247a7c305e9713b12d5501706296d
turnerdylan <dytu4626@colorado.edu>
2022-01-12 21:04:32 -0700

    Remove unneccesary logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js file loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
